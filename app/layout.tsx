3b432220a4c18c9ada43d328660da8ed1c831761
winsondong <winson@bu.edu>
2025-04-29 23:53:18 -0400

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "@/components/Header";
+import "./globals.css";
+
+
+export default function RootLayout(
+    {children,}:
+        Readonly<{children: React.ReactNode;}>
+){
+    return (
+        <html lang="en">
+            <body>
+                <Header />
+                {children}
+            </body>
+        </html>
+    )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    
+    return (
+        <header className="h-20 flex items-center justify-center">
+            <h2 className="text-4xl font-semibold p-4">CS391 OAuth</h2>
+        </header>
+    )
+}
